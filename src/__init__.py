816c41b794d66cdee3c077a9d0e2aa42d147a70c
Skilletpan <skilletpan.14@gmail.com>
2019-04-01 18:41:59 +0200

    Added commands to root init (public exposition)

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,32 @@
-# Indy Errors
+# Anoncreds
+from .commands.anoncreds import Anoncreds
+
+# Blob Storage
+from .commands.blob_storage import BlobStorage
+
+# Crypto
+from .commands.crypto import Crypto
+
+# DID
+from .commands.did import DID
+
+# Errors
 from .error import IndyError, IndyErrorCode
+
+# Ledger
+from .commands.ledger import Ledger
+
+# Non-Secrets
+from .commands.non_secrets import NonSecrets
+
+# Pairwise
+from .commands.pairwise import Pairwise
+
+# Payment
+from .commands.payment import Payment
+
+# Pool
+from .commands.pool import Pool
+
+# Wallet
+from .commands.wallet import Wallet
