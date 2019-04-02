6ee58cc23c4b7fce87868fa25cfdb75ac9b3ba3c
Skilletpan <skilletpan.14@gmail.com>
2019-04-02 18:47:12 +0200

    Fixed module references / imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,35 +1,8 @@
 # Libindy
 from .libindy import initialize_libindy
 
-# Anoncreds
-from .commands.anoncreds import Anoncreds
+# Libindy commands
+from .commands import Anoncreds, BlobStorage, Crypto, DID, Ledger, NonSecrets, Pairwise, Payment, Pool, Wallet
 
-# Blob Storage
-from .commands.blob_storage import BlobStorage
-
-# Crypto
-from .commands.crypto import Crypto
-
-# DID
-from .commands.did import DID
-
-# Errors
+# Libindy errors
 from .error import IndyError, IndyErrorCode
-
-# Ledger
-from .commands.ledger import Ledger
-
-# Non-Secrets
-from .commands.non_secrets import NonSecrets
-
-# Pairwise
-from .commands.pairwise import Pairwise
-
-# Payment
-from .commands.payment import Payment
-
-# Pool
-from .commands.pool import Pool
-
-# Wallet
-from .commands.wallet import Wallet
