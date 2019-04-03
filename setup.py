063ce472487dcae87ae1f05cf52a7795650b4a3a
Jeremy Roth <skilletpan.14@gmail.com>
2019-04-03 15:02:57 +0200

    Bugfix/module init (#6)
    * Attempt #6
    
     % Added package classifiers
    
    * Attempt #7
    
     % Added package programming language classifier
    
    * Attempt #8
    
     % Added License
     % Changed version number
    
    * Attempt #9
    
     % Changed version number and programming language version back

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(name='sbca-indy-wrapper',
-      version='1.8.1',  # Should be kept at the same version as the Libindy library
+      version='1.8.1-pre',
       description='A Python 3.6 wrapper for the Libindy library developed by Hyperledger.',
       url='https://github.com/swisscom-blockchain/sbca-indy-wrapper',
       author='Skilletpan',
       author_email='skilletpan.14@gmail.com',
       license='Apache 2.0',
       packages=find_packages(),
-      install_requires=[],
+      classifiers=[
+            'Programming Language :: Python :: 3.6',
+            'License :: OSI Approved :: Apache Software License',
+            'Operating System :: OS Independent'
+      ],
       zip_safe=False
       )
