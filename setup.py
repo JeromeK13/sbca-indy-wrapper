41702edd7eaf8736036cffd8cbc1134b456b6f9c
Skilletpan <skilletpan.14@gmail.com>
2019-04-03 10:24:31 +0200

    Revert "Attempt #4"
    This reverts commit 840395ca446b0e8a5a67f01cf45a6bd34241e0d9.

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(name='sbca-indy-wrapper',
       author_email='skilletpan.14@gmail.com',
       license='Apache 2.0',
       packages=find_packages(),
-      package_dir={'sbca_wrapper': 'src'},
       install_requires=[],
       zip_safe=False
       )
